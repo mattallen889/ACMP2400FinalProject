4812bc7a75df2eab1dd980d6ec23be4c9678088d
mattallen889 <mattallen889@gmail.com>
2026-04-23 10:57:57 -0500

    Refactor backend configuration in main.tf
    Removed duplicate backend configuration and ensured proper formatting.

--- final_app/stage1/main.tf
+++ final_app/stage1/main.tf
@@ -5,12 +5,12 @@ terraform {
       version = "4.69.0"
     }
   }
-}
-backend "azurerm" {
-  resource_group_name = "rg-acmp-final"
-  storage_account_name = "acmp2400storageaccount"
-  container_name = "big-tf-state-acmp2400"
-  use_azuread_auth = true
+  backend "azurerm" {
+    resource_group_name = "rg-acmp-final"
+    storage_account_name = "acmp2400storageaccount"
+    container_name = "big-tf-state-acmp2400"
+    use_azuread_auth = true
+  }
 }
 
 provider "azirerm" {
