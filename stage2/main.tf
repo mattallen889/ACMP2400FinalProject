f3d4c794c182a9f7227996341aa05c3baa77c0e3
mattallen889 <mattallen889@gmail.com>
2026-04-28 17:06:03 -0500

    Update main.tf

--- stage2/main.tf
+++ stage2/main.tf
@@ -10,11 +10,11 @@ terraform {
       version = "4.70.0"
     }
   }
-  backend "azurerm" {
-    resource_group_name = "rg-acmp-final"
-    storage_account_name = "acmp2400storageaccount"
-    container_name = "big-tf-state-acmp2400"
-    use_azuread_auth = true
+backend "azurerm" {
+  resource_group_name = "rg-acmp-final"
+  storage_account_name = "acmp2400storageaccount"
+  container_name = "big-tf-state-acmp2400"
+  use_azuread_auth = true
   }
 }
 
@@ -28,36 +28,35 @@ resource "azurerm_container_registry" "mattallen_acr" {
   location = "Central US"
   sku = "Basic"
   admin_enabled = false
-  }
+}
 
-resource "azurerm_container_group" "mattallen-aci" {
-  name = "acmp-mattallen-aci"
+#-----------------------------------------------------------
+
+resource "azurerm_container_group" "aci-mattallen-acmp" {
+  name = "aci-mattallen-acmp"
   location = "Central US"
   resource_group_name = "rg-mattallen"
   ip_address_type = "Public"
-  dns_name_label = "acmp-mattallen-instance"
+  dns_name_label = "aci-mattallen-acmp"
   os_type = "Linux"
 
   container {
-    name = "final"
-    image = "acrmattallenacmp2400.azurecr.io/final:latest"
+    name = "final-app"
+    image = "acrmattallen889acmp2400.azurecr.io/final:latest"
     cpu = "0.5"
     memory = "1.5"
 
-  ports {
-    port = 800
-    protocol = "TCP"
+    ports {
+      port = 8000
+      protocol = "TCP"
+    }
+    secure_environment_variables = {
+        DJANGO_SECRET_KEY = var.DJANGO_SECRET_KEY_PROD
   }
-
-  secure_environment_variables = {
-    DJANGO_SECRET_KEY = var.DJANGO_SECRET_KEY_PROD
-  }
-    
-}
-
+  
   image_registry_credentials {
-    server = "acrmattallenacmp2400.azurecr.io"
+    server = "acrmattallen889acmp2400.azurecr.io"
     username = var.ARM_CLIENT_ID
     password = var.ARM_CLIENT_SECRET
   }
-}
+}  
