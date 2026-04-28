69a96eb461382f8ae2465908fa831a1c481ef8b5
mattallen889 <mattallen889@gmail.com>
2026-04-27 20:06:20 -0500

    Simplify terraform plan and apply commands

--- .github/actions/terraform/entrypoint.sh
+++ .github/actions/terraform/entrypoint.sh
@@ -12,13 +12,13 @@ export DJANGO_SECRET_KEY_PROD=${INPUT_DJANGO_SECRET_KEY_PROD}
 
 if [[ "$TF_STAGE" == "final_app/stage1" ]]; then
   terraform -chdir=${INPUT_TF_STAGE} init -backend-config="key=${INPUT_STATE_KEY}.tfstate"
-  terraform -chdir=${INPUT_TF_STAGE} plan -out=${INPUT_TF_STAGE}.tfplan
-  terraform -chdir=${INPUT_TF_STAGE} apply ${INPUT_TF_STAGE}.tfplan
+  terraform -chdir=${INPUT_TF_STAGE} plan -out=tfplan
+  terraform -chdir=${INPUT_TF_STAGE} apply tfplan
 elif [[ "$TF_STAGE" == "final_app/stage2" ]]; then
   terraform -chdir=${INPUT_TF_STAGE} init -backend-config="key=${INPUT_STATE_KEY}.tfstate"
   terraform -chdir=${INPUT_TF_STAGE} apply -auto-approve -var="ARM_CLIENT_ID=${INPUT_ARM_CLIENT_ID}" -var="ARM_CLIENT_SECRET=${INPUT_ARM_CLIENT_SECRET}" -var="DJANGO_SECRET_KEY_PROD=${INPUT_DJANGO_SECRET_KEY_PROD}"
 elif [[ "$TF_STAGE" == "final_app/stage3" ]]; then
   terraform -chdir=${INPUT_TF_STAGE} init -backend-config="key=${INPUT_STATE_KEY}.tfstate"
-  terraform -chdir=${INPUT_TF_STAGE} plan -out=${INPUT_TF_STAGE}.tfplan
-  terraform -chdir=${INPUT_TF_STAGE} apply ${INPUT_TF_STAGE}.tfplan
+  terraform -chdir=${INPUT_TF_STAGE} plan -out=tfplan
+  terraform -chdir=${INPUT_TF_STAGE} apply tfplan
 fi
