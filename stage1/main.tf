2059e72bc3ab099a72b8f7365bae147b85f2e384
mattallen889 <mattallen889@gmail.com>
2026-04-28 16:05:16 -0500

    Update main.tf

--- stage1/main.tf
+++ stage1/main.tf
@@ -19,7 +19,7 @@ provider "azurerm" {
 
 resource "azurerm_container_registry" "mattallen_acr" {
   name = "acrmattallen889acmp2400"
-  resource_group_name = "rg-mattallen889" #changedfrommattallen
+  resource_group_name = "rg-mattallen" 
   location = "Central US"
   sku = "Basic"
   admin_enabled = false
